33b088d1a4c63f4efa6b334d2b0956e49a4050ea
vijaya8118 <140912151+vijaya8118@users.noreply.github.com>
2025-05-10 20:21:29 +0530

    Update settings.py

--- scoprj/settings.py
+++ scoprj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = ['.vercel.app',]
 # scoruda.localhost
 BASE_URL = 'localhost'
 LOGIN_URL = '/create_member/'
